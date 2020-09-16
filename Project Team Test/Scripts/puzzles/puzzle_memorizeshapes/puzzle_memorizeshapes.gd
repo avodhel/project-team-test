65915755cdc697ced8f69c4ee786d351725ce486
Ümitcan Sevinç <umitcansevinc91@gmail.com>
2020-09-16 16:45:23 +0300

    some project folders replaced and contents renamed

---
+++ Project Team Test/Scripts/puzzles/puzzle_memorizeshapes/puzzle_memorizeshapes.gd
@@ -0,0 +1,26 @@
+extends "res://Scripts/puzzles/puzzle_memorize.gd"
+
+onready var memorize_shapes = $memorize_shapes
+onready var shapes = $memorize_shapes/shapes
+
+func _ready():
+	#fill shape datas array from shape nodes
+	for data in shapes.get_children():
+		data_container.append(data)
+	memorize_shapes.visible = true
+	_puzzle_creater()
+
+#select randomly between two questions
+func _question_selector() -> void:
+	memorize_shapes.visible = false
+	._question_selector()
+	if ins_question.has_signal("result"):
+		ins_question.connect("result", self, "puzzle_checker")
+	if "unused_shape_textures" in ins_question:
+		ins_question.unused_shape_textures = memorize_shapes.texture_container
+	#get all color datas and fill container
+	var color_container = []
+	for no in memorize_shapes.colors.size():
+		color_container.append(memorize_shapes.colors[no])
+	ins_question.create_question(data_container, color_container)
+	ins_question.visible = true
---
+++ Project Team Test/Scripts/puzzles/puzzle_memorizeshapes/questions/question2_findshapescolor.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+signal result(event)
+
+export (String) var describe_text
+
+onready var sprite = $shape_sprite
+onready var color_circles = $color_circles
+
+var choosen_shape_color
+
+func create_question(data_list, color_list):
+	_prepare_shape(data_list)
+	_prepare_answers(color_list)
+
+#preapare question's shape according to shape data
+func _prepare_shape(shape_datas) -> void:
+	randomize()
+	var choosen_shape_data = shape_datas[randi() % shape_datas.size()]
+	sprite.texture = choosen_shape_data.shape_texture
+	choosen_shape_color = choosen_shape_data.shape_color
+
+#prepare clickable color options
+func _prepare_answers(colors) -> void:
+	randomize()
+	for color_circle in color_circles.get_children():
+		var choosen_color = colors[randi() % colors.size()]
+		color_circle.prepare_color_circle(choosen_color)
+		colors.erase(choosen_color)
+		color_circle.connect("clicked", self, "check_answer" )
+
+#check if clicked color and shape's color matched
+func check_answer(color) -> void:
+	if choosen_shape_color == color:
+		emit_signal("result", "correct")
+	else:
+		emit_signal("result", "wrong")
