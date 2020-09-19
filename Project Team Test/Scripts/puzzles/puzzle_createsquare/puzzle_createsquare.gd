66803954a211a1bd9fa088970913e1d3f819dcb0
Ümitcan Sevinç <umitcansevinc91@gmail.com>
2020-09-19 16:22:16 +0300

    time limit added to puzzle of create a square

--- Project Team Test/Scripts/puzzles/puzzle_createsquare/puzzle_createsquare.gd
+++ Project Team Test/Scripts/puzzles/puzzle_createsquare/puzzle_createsquare.gd
@@ -1,12 +1,15 @@
 extends "res://Scripts/puzzles/puzzle.gd"
 
+onready var limit_timer = $limit_timer
 onready var pieces = $square_pieces
 
 var degrees = [0, 90, 180, 270]
 var success_controls = []
+var puzzle_completed_control = false
 
 func _ready():
 	_puzzle_creater()
+	limit_timer.start()
 
 func _puzzle_creater() -> void:
 	emit_signal("puzzle_started", describe_text)
@@ -23,4 +26,10 @@ func puzzle_checker() -> void:
 	for piece in pieces.get_children():
 		success_controls.append(piece.success_control)
 	if success_controls.has(false) == false:
+		puzzle_completed_control = true
 		emit_signal("puzzle_finished", "create_new_puzzle", "successful")
+
+#when limit time to finish puzzle is over and puzzle isn't still completed
+func _on_limit_timer_timeout():
+	if puzzle_completed_control == false:
+		emit_signal("puzzle_finished", "create_new_puzzle", "failed")
